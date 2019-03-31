943a483014bf5de351a5f2f44c523af516047ba9
Min Bai <mbai3@uber.com>
2019-03-31 17:59:46 -0400

    Adding new experiment

--- fusion_experiments/experiments/fusion_baselines/__init__.py
+++ fusion_experiments/experiments/fusion_baselines/__init__.py
@@ -10,4 +10,5 @@ from __future__ import absolute_import
 import experiments.fusion_baselines.train_fusion_baselines
 import experiments.fusion_baselines.train_volumetric_fusion
 import experiments.fusion_baselines.train_cross_sectional_fusion
+import experiments.fusion_baselines.train_cross_sectional_attention_fusion
 
